b66b43b8713dcf85516ebc6d57ffba3388c8d102
Leonardo Ferreira <leovsferreira@gmail.com>
2023-06-14 17:23:45 -0300

    fixing model gallery problem

--- front/src/app/image-gallery/image-gallery.component.ts
+++ front/src/app/image-gallery/image-gallery.component.ts
@@ -61,6 +61,7 @@ export class ImageGalleryComponent implements OnInit {
     this.allImages = [];
     this.items = [];
     this.selectedIndices = [];
+    this.selectedImagePaths = [];
 
     const similarities = data.similarities;
     const paths = data.labelPaths;
@@ -146,6 +147,8 @@ export class ImageGalleryComponent implements OnInit {
 
   selectImages(points: any) {
     this.selectedIndices = [];
+    this.selectedImagePaths = [];
+    
     for(let i = 0; i < this.allImages.length; i++) {
       this.allImages[i].border = 'none';
       this.allImages[i].borderColor = "";
